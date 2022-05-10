c3dcd1adf93e261e5069f021927f9ac0001d94d2
diegofdg <diegofdg@gmail.com>
2022-05-10 15:34:31 -0300

    ejercicio_07

---
+++ ejercicio_07.sql
@@ -0,0 +1,5 @@
+#* Consulta en la tabla INVENTORY cuales son los registros cuyo film_id sea mayor a 50 *#
+SELECT * FROM sakila.inventory WHERE film_id > 50;
+
+#* Consulta en la tabla PAYMENT cuales son los registros cuyo amount sea menor a 3 *#
+SELECT * FROM sakila.payment WHERE amount < 3;
