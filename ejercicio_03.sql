f8050cef917446ee6d4acc37b4ae7a4bbe6367c4
diegofdg <diegofdg@gmail.com>
2022-05-10 00:16:17 -0300

    ejercicio_03

---
+++ ejercicio_03.sql
@@ -0,0 +1,5 @@
+#* Mostrar el listado completo de los registros de la tabla COUNTRY ordenados de manera ascendente por la columna country *#
+SELECT * FROM sakila.country ORDER BY country ASC;
+
+#* Mostrar el listado completo de los registros de la tabla CUSTOMER ordenados de manera descendente por la columna first_name *#
+SELECT * FROM sakila.customer ORDER BY first_name DESC;
