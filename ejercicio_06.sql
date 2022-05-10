5b55fdf204c84c5ff0ee4c4017d4671dbea88f38
diegofdg <diegofdg@gmail.com>
2022-05-10 00:48:06 -0300

    ejercicio_06

---
+++ ejercicio_06.sql
@@ -0,0 +1,8 @@
+#* Consulta en la tabla ACTOR cuales son los registros cuyo first_name sea igual a "DAN" *#
+SELECT * FROM sakila.actor WHERE first_name = "DAN";
+
+#* Consulta en la tabla CITY cuales son los registros cuyo city sea igual a "LONDON" *#
+SELECT * FROM sakila.city WHERE city = "LONDON";
+
+#* Consulta en la tabla CUSTOMER cuales son los registros cuyo store_id sea igual a 1 *#
+SELECT * FROM sakila.customer WHERE store_id = 1;
