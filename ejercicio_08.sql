6f136f6d314f174964154890aec949226199253b
diegofdg <diegofdg@gmail.com>
2022-05-10 15:40:10 -0300

    ejercicio_08

---
+++ ejercicio_08.sql
@@ -0,0 +1,5 @@
+#* Consulta en la tabla STAFF cuales son los registros cuyo staff_id sea diferente de 2 *#
+SELECT * FROM sakila.staff WHERE staff_id <> 2;
+
+#* Consulta en la tabla LANGUAGE cuales son los registros cuyo name sea diferente de "GERMAN" *#
+SELECT * FROM sakila.language WHERE name <> "GERMAN";
