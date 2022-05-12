7be425073daecbbf40d6fa8ca11ec57df55e2027
diegofdg <diegofdg@gmail.com>
2022-05-12 00:22:29 -0300

    ejercicio_25

---
+++ ejercicio_25.sql
@@ -0,0 +1,2 @@
+#* Consulta en la tabla FILM los registros donde rental_rate esté entre 0.99 y 2.99, length sea menor igual de 50  y replacement_cost sea menor de 20 *#
+SELECT * FROM sakila.film WHERE (rental_rate BETWEEN 0.99 AND 2.99) AND length <= 50 AND replacement_cost < 20;
