8fa8902f1ec3b921a0d07042da61e788e6a352d7
diegofdg <diegofdg@gmail.com>
2022-05-12 00:06:20 -0300

    ejercicio_23

---
+++ ejercicio_23.sql
@@ -0,0 +1,2 @@
+#* Consulta en la tabla PAYMENT los registros donde amount esté entre 2.99 y 4.99, staff_id sea igual a 2 y customer_id sea 1 y 2 *#
+SELECT * FROM sakila.payment WHERE (amount BETWEEN 2.99 AND 4.99) AND staff_id = 2 AND customer_id IN (1,2);
