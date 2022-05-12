06e2696c5c34bb664f3c6d23b3b8d0ff1ace6527
diegofdg <diegofdg@gmail.com>
2022-05-12 20:54:13 -0300

    ejercicio_38

---
+++ ejercicio_38.sql
@@ -0,0 +1,2 @@
+#* Consulta la tabla RENTAL, realiza un INNER JOIN con la tabla STAFF y mostrar las columnas rental_id, first_name *#
+SELECT r.rental_id, s.first_name FROM sakila.rental r INNER JOIN sakila.staff s ON (r.staff_id = s.staff_id);
