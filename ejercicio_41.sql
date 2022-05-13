70e1bd018e07208c0cce310e834f5c01017d430a
diegofdg <diegofdg@gmail.com>
2022-05-12 21:39:25 -0300

    ejercicio_41

---
+++ ejercicio_41.sql
@@ -0,0 +1,5 @@
+#* Cuenta la cantidad de registros de la tabla ACTOR *#
+SELECT COUNT(*) FROM sakila.actor;
+
+#* Cuenta la cantidad de registros de la columna first_name de la tabla ACTOR *#
+SELECT COUNT(first_name) FROM sakila.actor;
