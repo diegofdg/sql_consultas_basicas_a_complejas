75179974fb240498f134d2f173b1ebbd0a42b59e
diegofdg <diegofdg@gmail.com>
2022-05-13 05:53:22 -0300

    ejercicio_42

---
+++ ejercicio_42.sql
@@ -0,0 +1,5 @@
+#* Calcula el promedio de la columna amount de la tabla PAYMENT *#
+SELECT AVG(amount) FROM sakila.payment;
+
+#* Calcula el promedio de la columna rental_duration de la tabla FILM *#
+SELECT AVG(rental_duration) FROM sakila.film;
