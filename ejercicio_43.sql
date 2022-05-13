613d94053b187fa3c37245e29d13ec840eeb16a3
diegofdg <diegofdg@gmail.com>
2022-05-13 06:00:02 -0300

    ejercicio_43

---
+++ ejercicio_43.sql
@@ -0,0 +1,11 @@
+#* Calcula cual es el máximo valor de la columna length de la tabla FILM *#
+SELECT MAX(length) FROM sakila.film;
+
+#* Calcula cual es el mínimo valor de la columna length de la tabla FILM *#
+SELECT MIN(length) FROM sakila.film;
+
+#* Calcula cual es el mínimo valor de la columna replacement_cost de la tabla FILM *#
+SELECT MIN(replacement_cost) FROM sakila.film;
+
+#* Calcula cual es el máximo valor de la columna replacement_cost de la tabla FILM *#
+SELECT MAX(replacement_cost) FROM sakila.film;
