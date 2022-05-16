843d1580f5ece74a3942685cc5a0b664b64da741
diegofdg <diegofdg@gmail.com>
2022-05-15 21:24:14 -0300

    ejercicio_82

---
+++ 16_insert_into/ejercicio_82.sql
@@ -0,0 +1,2 @@
+#* Inserta un registro en la tabla CUSTOMER #*
+INSERT INTO sakila.customer (store_id, first_name, last_name, email, address_id, active, create_date) VALUES ('1', 'nombre', 'apellido', 'correo@correo.com', '5', '1', '2022-05-16 02:21:09.000000');
